0d72d8d6ac8c77c0bad402f1ba1c22e5bce85046
gwenrino <gwenrino@gmail.com>
2018-04-13 17:23:46 -0700

    Setting up project template

--- dengue/munge/01-A.R
+++ dengue/munge/01-A.R
@@ -1,5 +1,14 @@
 # Add target labels to training dataset
 dengue.data <- left_join(dengue.features.train, dengue.labels.train)
 
+# Add season variable
+dengue.data <- dengue.data %>% mutate(season = 
+                                        case_when(weekofyear >= 1 & weekofyear <= 12 ~ 'winter',
+                                                  weekofyear >= 13 & weekofyear <= 25 ~ 'spring',
+                                                  weekofyear >= 26 & weekofyear <= 38 ~ 'summer',
+                                                  weekofyear >= 39 & weekofyear <= 51 ~ 'fall',
+                                                  weekofyear >= 52 ~ 'winter')
+                                      )
+
 # Filter out San Juan data
 dengue.sj <- filter(dengue.data, city == "sj")
---
+++ dengue/src/Naive.R
@@ -0,0 +1,24 @@
+## Naive model = mean total_cases by season
+
+# Find mean total_cases for each season
+dengue.sj %>% group_by(season) %>% 
+  summarize(mean = mean(total_cases, na.rm = TRUE))
+
+# Assign seasonal mean as prediction for each observation
+dengue.sj.naive <- dengue.sj %>% 
+  mutate(predictions = case_when(season == "fall" ~ 59.2,
+                                 season == "spring" ~ 11.9,
+                                 season == "summer" ~ 41.6,
+                                 season == "winter" ~ 23.9))
+
+# Error = actual number of cases - predicted number of cases
+error <- dengue.sj.naive$total_cases - dengue.sj.naive$predictions
+
+# Function that returns Mean Absolute Error
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+# Find MAE for naive model
+mae(error)
