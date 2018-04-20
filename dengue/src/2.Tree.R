947008581b8a6870754e253383f338ba742e5428
gwenrino <gwenrino@gmail.com>
2018-04-20 12:37:38 -0700

    Linear and tree models, performance

---
+++ dengue/src/2.Tree.R
@@ -0,0 +1,25 @@
+## Tree model using all features except week_start_date
+
+# Create dataset with selected features
+dengue.small <- dengue.sj %>% select(-year, -week_start_date)
+
+# Create training set
+set.seed(123)
+train_set <- dengue.small %>% sample_frac(0.7)
+# Create test set
+test_set <- anti_join(dengue.small, train_set)
+
+# Fit model
+tree_1 <- rpart(total_cases ~ ., data = train_set)
+
+# Visualize tree
+plot(as.party(tree_1))
+
+# Predictions from tree_1
+predictions.tree_1 <- predict(tree_1, newdata = test_set)
+
+# Error = actual number of cases - predicted number of cases
+error <- test_set$total_cases - predictions.tree_1
+
+# Find MAE
+mae(error)
