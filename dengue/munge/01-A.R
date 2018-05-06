44c810e9e1c8982b1316688693343125fb3f821c
gwenrino <gwenrino@gmail.com>
2018-05-06 13:52:40 -0700

    Add new data; create preprocessed datasets

--- dengue/munge/01-A.R
+++ dengue/munge/01-A.R
@@ -20,3 +20,33 @@ dengue.sj <- subset(dengue.sj, select = -city)
 # Convert week_start_date to date object
 dengue.sj$week_start_date <- as.Date(dengue.sj$week_start_date, format = '%Y-%m-%d')
 
+## MUNGING TO ADD DATA FROM NEW SOURCE
+
+# Convert month_year in new data to yearmon using zoo
+hotel.guests.copy$month_year <- as.yearmon(hotel.guests.copy$month_year, "%b-%y")
+
+# Make new column month_year
+dengue.sj$month_year <- format(as.Date(dengue.sj$week_start_date), "%b-%y")
+
+# Convert that to yearmon using zoo
+dengue.sj$month_year <- as.yearmon(dengue.sj$month_year, "%b-%y")
+
+# Join new data using month_year yearmon object as key
+dengue.sj <- left_join(dengue.sj, hotel.guests.copy, by = "month_year")
+
+# Reorder columns so target is last
+dengue.sj <- dengue.sj[ ,c(1:23,25,26,27,24)]
+
+## CREATE CLEAN, CENTERED, SCALED VERSIONS FOR REGRESSION MODELS
+
+prepParams.1 <- preProcess(dengue.sj[ ,1:26], method = c("medianImpute", "center", "scale"))
+
+dengue.med <- predict(prepParams.1, dengue.sj[ ,1:26])
+
+dengue.med$total_cases <- dengue.sj$total_cases
+
+prepParams.2 <- preProcess(dengue.sj[ ,1:26], method = c("knnImpute", "center", "scale"))
+
+dengue.knn <- predict(prepParams.2, dengue.sj[ ,1:26])
+
+dengue.knn$total_cases <- dengue.sj$total_cases
