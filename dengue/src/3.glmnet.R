ae3e8b144daa31756f24e06cb3f76eaf38bc03cd
gwenrino <gwenrino@gmail.com>
2018-05-06 14:11:35 -0700

    glmnet model

--- dengue/src/3.glmnet.R
+++ dengue/src/3.glmnet.R
@@ -3,7 +3,7 @@ load.project()
 
 # With NAs imputed with median values
 
-glmnet_1 <- train(total_cases ~ .,
+glmnet_1 <- train(total_cases ~ . -week_start_date,
                   data = dengue.med,
                   method = "glmnet")
 
@@ -11,12 +11,12 @@ print(glmnet_1)
 
 # With NAs imputed with knn values
 
-glmnet_2 <- train(total_cases ~ .,
+glmnet_2 <- train(total_cases ~ . -week_start_date,
                   data = dengue.knn,
                   method = "glmnet")
 
 print(glmnet_2)
 
 # Best values for both models were alpha = 0.1 and lambda = 0.5350029.
-# MAE for best glmnet_1 was 10.05.
-# MAE for best glmnet_2 was 9.71.
+# MAE for best glmnet_1 was 9.95.
+# MAE for best glmnet_2 was 9.55.
