e3f40ac629be61a8e33c2ecf0605199fc0f9b0d4
gwenrino <gwenrino@gmail.com>
2018-05-08 15:16:40 -0700

    Add dengue.data package

---
+++ dengue/pkgs/dengue.data/data-raw/dengue.munge.R
@@ -0,0 +1,69 @@
+# Read in raw data
+dengue.features.train <- 
+  read.csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data/data-raw/dengue_features_train.csv", header = TRUE)
+dengue.labels.train <- 
+  read.csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data/data-raw/dengue_labels_train.csv", header = TRUE)
+hotel.guests.copy <- 
+  read.csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data/data-raw/hotel_guests_copy.csv", header = TRUE)
+
+# Add target labels to training dataset
+dengue.data <- left_join(dengue.features.train, dengue.labels.train)
+
+# Add season variable
+dengue.data <- dengue.data %>% 
+  mutate(season = 
+           case_when(weekofyear >= 1 & weekofyear <= 12 ~ 'winter',
+                     weekofyear >= 13 & weekofyear <= 25 ~ 'spring',
+                     weekofyear >= 26 & weekofyear <= 38 ~ 'summer',
+                     weekofyear >= 39 & weekofyear <= 51 ~ 'fall',
+                     weekofyear >= 52 ~ 'winter')
+  )
+
+# Filter out San Juan data
+dengue.sj <- filter(dengue.data, city == "sj")
+
+# Remove city variable
+dengue.sj <- subset(dengue.sj, select = -city)
+
+# Convert week_start_date to date object
+dengue.sj$week_start_date <- as.Date(dengue.sj$week_start_date, format = '%Y-%m-%d')
+
+## MUNGING TO ADD DATA FROM NEW SOURCE
+
+# Convert month_year in new data to yearmon using zoo
+hotel.guests.copy$month_year <- as.yearmon(hotel.guests.copy$month_year, "%b-%y")
+
+# Make new column month_year
+dengue.sj$month_year <- format(as.Date(dengue.sj$week_start_date), "%b-%y")
+
+# Convert that to yearmon using zoo
+dengue.sj$month_year <- as.yearmon(dengue.sj$month_year, "%b-%y")
+
+# Join new data using month_year yearmon object as key
+dengue.sj <- left_join(dengue.sj, hotel.guests.copy, by = "month_year")
+
+# Reorder columns so target is last
+dengue.sj <- dengue.sj[ ,c(1:23,25,26,27,24)]
+
+## CREATE CLEAN, CENTERED, SCALED VERSIONS FOR REGRESSION MODELS
+
+# With median imputation of missing values
+prepParams.1 <- preProcess(dengue.sj[ ,1:26], method = c("medianImpute", "center", "scale"))
+dengue.med <- predict(prepParams.1, dengue.sj[ ,1:26]) # Create preprocessed dataset
+dengue.med$total_cases <- dengue.sj$total_cases # Add target variable
+
+# With knn imputation of missing values
+prepParams.2 <- preProcess(dengue.sj[ ,1:26], method = c("knnImpute", "center", "scale"))
+dengue.knn <- predict(prepParams.2, dengue.sj[ ,1:26]) # Create preprocessed dataset
+dengue.knn$total_cases <- dengue.sj$total_cases # Add target variable
+
+# Add processed data to package
+devtools::use_data(dengue.sj, 
+                   pkg = "~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data",
+                   overwrite = TRUE)
+devtools::use_data(dengue.med, 
+                   pkg = "~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data",
+                   overwrite = TRUE)
+devtools::use_data(dengue.knn, 
+                   pkg = "~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/pkgs/dengue.data",
+                   overwrite = TRUE)
