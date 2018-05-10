8c75289df52fce286ab3ff5c63d266a2cd6b57f4
gwenrino <gwenrino@gmail.com>
2018-05-10 12:22:43 -0700

    Revise to reflect updated munging

--- dengue/src/Models/0.Naive.R
+++ dengue/src/Models/0.Naive.R
@@ -4,18 +4,18 @@ load.project()
 ## Naive model = mean total_cases by season
 
 # Find mean total_cases for each season
-dengue.sj %>% group_by(season) %>% 
+dengue %>% group_by(season) %>% 
   summarize(mean = mean(total_cases, na.rm = TRUE))
 
 # Assign seasonal mean as prediction for each observation
-dengue.sj.naive <- dengue.sj %>% 
+dengue.naive <- dengue %>% 
   mutate(predictions = case_when(season == "fall" ~ 59.2,
                                  season == "spring" ~ 11.9,
                                  season == "summer" ~ 41.6,
                                  season == "winter" ~ 23.9))
 
 # Error = actual number of cases - predicted number of cases
-error <- dengue.sj.naive$total_cases - dengue.sj.naive$predictions
+error <- dengue.naive$total_cases - dengue.naive$predictions
 
 # Find MAE for naive model
 mae(error)
--- dengue/src/Models/1.Linear.R
+++ dengue/src/Models/1.Linear.R
@@ -5,19 +5,17 @@ load.project()
 ## (excluding engineered feature year.season)
 
 # Create dataset with selected features
-dengue.small.1 <- dengue.sj %>% select(ndvi_nw, reanalysis_air_temp_k:reanalysis_min_air_temp_k,
+# Data scaled, centered; missing values imputed with median values
+dengue.med.sm <- dengue.med.scaled %>% select(ndvi_nw, reanalysis_air_temp_k:reanalysis_min_air_temp_k,
                                      reanalysis_relative_humidity_percent, reanalysis_specific_humidity_g_per_kg,
                                      station_avg_temp_c, station_max_temp_c, station_min_temp_c, total_cases, 
                                      weekofyear)
 
-# Eliminate NAs
-dengue.sm.1.clean <- na.omit(dengue.small.1)
-
 # Create training set
 set.seed(555)
-train_set.1 <- dengue.sm.1.clean %>% sample_frac(0.7)
+train_set.1 <- dengue.med.sm %>% sample_frac(0.7)
 # Create test set
-test_set.1 <- anti_join(dengue.sm.1.clean, train_set.1)
+test_set.1 <- anti_join(dengue.med.sm, train_set.1)
 
 # Fit model
 lm_1 <- lm(total_cases ~ ., data = train_set.1)
@@ -33,24 +31,18 @@ error1 <- test_set.1$total_cases - predictions.lm_1
 # Find MAE
 mae(error1)
 
-
-## Linear model using features with significant p-values in Pearson tests
-## (including engineered feature year.season)
-
 # Create dataset with selected features
-dengue.small.2 <- dengue.sj %>% select(ndvi_nw, reanalysis_air_temp_k:reanalysis_min_air_temp_k,
-                                       reanalysis_relative_humidity_percent, reanalysis_specific_humidity_g_per_kg,
-                                       station_avg_temp_c, station_max_temp_c, station_min_temp_c, total_cases, 
-                                       weekofyear, year.season)
-
-# Eliminate NAs
-dengue.sm.2.clean <- na.omit(dengue.small.2)
+# Data scaled, centered; missing values imputed with knn values
+dengue.knn.sm <- dengue.knn.scaled %>% select(ndvi_nw, reanalysis_air_temp_k:reanalysis_min_air_temp_k,
+                                              reanalysis_relative_humidity_percent, reanalysis_specific_humidity_g_per_kg,
+                                              station_avg_temp_c, station_max_temp_c, station_min_temp_c, total_cases, 
+                                              weekofyear)
 
 # Create training set
-set.seed(444)
-train_set.2 <- dengue.sm.2.clean %>% sample_frac(0.7)
+set.seed(777)
+train_set.2 <- dengue.knn.sm %>% sample_frac(0.7)
 # Create test set
-test_set.2 <- anti_join(dengue.sm.2.clean, train_set.2)
+test_set.2 <- anti_join(dengue.knn.sm, train_set.2)
 
 # Fit model
 lm_2 <- lm(total_cases ~ ., data = train_set.2)
@@ -65,4 +57,3 @@ error2 <- test_set.2$total_cases - predictions.lm_2
 
 # Find MAE
 mae(error2)
-
