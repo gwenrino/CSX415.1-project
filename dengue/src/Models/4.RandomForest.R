3add39380842d1497c04c31dd5e5699a420fa973
gwenrino <gwenrino@gmail.com>
2018-05-13 14:50:13 -0700

    Update random forest model and eval

--- dengue/src/Models/4.RandomForest.R
+++ dengue/src/Models/4.RandomForest.R
@@ -1,23 +1,33 @@
 library('ProjectTemplate')
 load.project()
 
-# DoParallel
+# Do parallel
 cl <- makeCluster(detectCores())
 registerDoParallel(cl)
 
 # Training control
-myControl <- trainControl(method = "cv", number = 5, allowParallel = TRUE, verboseIter = TRUE)
+myControl <- trainControl(method = "cv", number = 5, verboseIter = TRUE, allowParallel = TRUE)
 
-## RF with NAs imputed with median values and default mtry and ntree values
-set.seed(777)
+# With NAs imputed with median values and default mtry and ntree
+set.seed(555)
 rf_1 <- train(total_cases ~ . -week_start_date, 
               data = dengue.med, 
               method = "rf",
-              trControl = myControl)
+              trControl = myControl
+              )
 
 print(rf_1)
 
-# Best model has MAE of 14.5
+
+# With NAs imputed with knn values and default mtry and ntree
+set.seed(565)
+rf_2 <- train(total_cases ~ . -week_start_date, 
+              data = dengue.knn, 
+              method = "rf",
+              trControl = myControl
+)
+
+print(rf_2)
 
 stopCluster(cl)
 
