9e9df0ba6c79a05cf35ce2e02e950cb7d113d94d
gwenrino <gwenrino@gmail.com>
2018-05-15 12:31:36 -0700

    Use med instead of knn imputation

--- dengue/munge/01-A.R
+++ dengue/munge/01-A.R
@@ -78,3 +78,10 @@ dengue.ts.target <- ts(dengue$total_cases,
 # Time series of first differences
 dengue.ts.target.diff <- diff(dengue.ts.target, lag = 52)
 
+# Time series of selected features
+selected <- dengue.med[c("total_cases", "nonres_guests", "station_max_temp_c", 
+                         "reanalysis_tdtr_k", "reanalysis_dew_point_temp_k",
+                         "reanalysis_specific_humidity_g_per_kg")]
+ts.selected <- ts(selected,
+                  freq = 365.25/7,
+                  start = decimal_date(ymd("1990-05-07")))
