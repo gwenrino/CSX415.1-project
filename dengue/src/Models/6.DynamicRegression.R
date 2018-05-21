a0ff26e5f93119d7772cb7d6b7feac5993540660
gwenrino <gwenrino@gmail.com>
2018-05-21 15:03:32 -0700

    Add forecast of reanalysis_dew_point_temp_k for use as xreg in model

--- dengue/src/Models/6.DynamicRegression.R
+++ dengue/src/Models/6.DynamicRegression.R
@@ -286,4 +286,79 @@ mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
 model <- auto.arima(train2[,"total_cases"], xreg = v4.1)
 fc <- forecast(model, xreg = rep(d.1,26))
 
-autoplot(fc, xlim = c(2005-01,2009)) + autolayer(test2[,"total_cases"], series = "Test Data")
+###############################################
+### Forecasting reanalysis_dew_point_temp_k ###
+### for use in xreg forecasting             ###
+###############################################
+
+# Model is an ARIMA(1,1,1) with reanalysis_dew_point_temp_k as regressor.
+# In order to forecast total_cases, need forecasts of reanalysis_dew_point_temp_k
+
+# Create time series of features for use in model, and train and test sets
+final <- dengue.med[c("total_cases", "reanalysis_dew_point_temp_k")]
+
+ts.final <- ts(final,
+               freq = 365.25/7,
+               start = decimal_date(ymd("1990-05-07")))
+
+train <- subset(ts.final, start = 1, end = 930)
+test <- subset(ts.final, start = 931, end = 936)
+
+## NAIVE MODEL
+
+# Fit model
+dewpt.mod1 <- naive(ts.final[,"reanalysis_dew_point_temp_k"])
+summary(dewpt.mod1)
+
+train[,"reanalysis_dew_point_temp_k"] %>% naive() %>%
+  forecast(h=6) %>% accuracy(test[,"reanalysis_dew_point_temp_k"])
+# MAE on this test set = 0.64
+
+# Cross validate using tsCV()
+e <- tsCV(ts.final[,"reanalysis_dew_point_temp_k"], forecastfunction = naive, h=6)
+mean(abs(e), na.rm = TRUE) # MAE is 0.81
+
+## SEASONAL NAIVE MODEL
+
+# Fit model
+dewpt.mod2 <- snaive(ts.final[,"reanalysis_dew_point_temp_k"])
+summary(dewpt.mod2)
+
+train[,"reanalysis_dew_point_temp_k"] %>% snaive() %>%
+  forecast(h=6) %>% accuracy(test[,"reanalysis_dew_point_temp_k"])
+# MAE on this test set = 1.33
+
+# Cross validate using tsCV()
+e <- tsCV(ts.final[,"reanalysis_dew_point_temp_k"], forecastfunction = snaive, h=6)
+mean(abs(e), na.rm = TRUE) # MAE is 0.78
+
+# Use seasonal naive forecasts of reanalysis_dew_point_temp_k as
+# xreg in forecast of total_cases using ARIMA(1,1,1) model
+
+# Model and forecast reanalysis_dew_point_temp_k, save forecast results
+dewpt.fc <- train[,"reanalysis_dew_point_temp_k"] %>% snaive() %>% forecast(h=6)
+
+# Extract point values, assign to ptval
+ptval <- dewpt.fc[["mean"]]
+
+# Fit model, find accuracy of forecast of test set using ptval as xreg for forecasts
+train[,"total_cases"] %>% 
+  auto.arima(xreg = train[,"reanalysis_dew_point_temp_k"]) %>% 
+  forecast(xreg = rep(ptval,6)) %>% 
+  accuracy(test[,"total_cases"]) # MAE = 0.66
+
+# Cross validate this model using ro() function from greybox
+x <- ts.final[,"total_cases"]
+xreg <- ts.final[,"reanalysis_dew_point_temp_k"]
+
+ourCall <- "predict(arima(x=data, order=c(1,1,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
+ourValue <- "pred"
+
+returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
+
+mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 7.98
+
+# Models with all data 
+dengue.model <- auto.arima(ts.final[,"total_cases"], xreg = ts.final[,"reanalysis_dew_point_temp_k"])
+dewpt.model <- snaive(ts.final[,"reanalysis_dew_point_temp_k"])
