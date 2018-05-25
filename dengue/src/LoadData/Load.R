122e18d8d245645c99c3d8cfba4acfcaeb2a6068
gwenrino <gwenrino@gmail.com>
2018-05-25 10:31:11 -0700

    Tidy up

--- dengue/src/LoadData/Load.R
+++
@@ -1,6 +0,0 @@
-library('ProjectTemplate')
-load.project()
-
-dengue.features.train <- read_csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/data/dengue_features_train.csv")
-dengue.labels.train <- read_csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/data/dengue_labels_train.csv")
-hotel.guests.copy <- read_csv("~/Documents/Data_Science_Learning/CompSci415.1/CSX415.1-project/dengue/data/hotel_guests_copy.csv")
