95639f4deb8f20de8b2e907df28f5dc55d7d3115
gwenrino <gwenrino@gmail.com>
2018-05-25 12:06:33 -0700

    Add details to documentation of package

--- dengue/pkgs/Dengue/R/DengueForecast.R
+++ dengue/pkgs/Dengue/R/DengueForecast.R
@@ -1,42 +1,49 @@
 #' @title Dengue Forecast
 #' 
-#' 
 #' @param h forecast horizon in weeks (a number greater than 0)
-#' @examples DengueFC(10) # Returns forecasts for the next 10 weeks
+#' @examples DengueFC(10) # Returns dengue case forecasts and recommended staffing levels for the next 10 weeks
+#' 
+#' @details The function returns six pieces of information:  \cr
+#' *Forecast* is the number of dengue cases for that week as forecast by the model.  \cr
+#' *Lo95* and *Hi95* show the range of the 95 percent confidence interval of the dengue forecasts for that week.  \cr
+#' *StaffRec* is the number of health care workers needed to maintain the required 1:9 staffing ratio for the forecasted number of cases that week.  \cr
+#' *StaffLo* and *StaffHi* show the range of number of health care workers suggested by the 95 percent confidence interval of the dengue forecasts.
+#' 
 #' @import dplyr
 #' @import forecast
 #' @export
+#' @md
 
 DengueFC <- function(h){
   
   Week <- paste("Week", 1:h)
   ptval <- forecast::forecast(dewpt.model, h=h)[["mean"]] 
   
-  ForecastLo <- round(forecast::forecast(dengue.model, xreg = rep(ptval))$lower[,2])
-  ForecastLo[ForecastLo<0] <- 0
+  Lo95 <- round(forecast::forecast(dengue.model, xreg = rep(ptval))$lower[,2])
+  Lo95[Lo95<0] <- 0
   StaffLo <-  dplyr::case_when(
-    ForecastLo == 0 ~ as.numeric(1),
-    ForecastLo%%9 == 0 ~ as.numeric(ForecastLo/9),
-    ForecastLo%%9 != 0 ~ as.numeric(ForecastLo%/%9 + 1)
+    Lo95 == 0 ~ as.numeric(1),
+    Lo95%%9 == 0 ~ as.numeric(Lo95/9),
+    Lo95%%9 != 0 ~ as.numeric(Lo95%/%9 + 1)
   )
   
-  ForecastMid <- round(forecast::forecast(dengue.model, xreg = rep(ptval))[["mean"]])
-  ForecastMid[ForecastMid<0] <- 0
-  StaffMid <-  dplyr::case_when(
-    ForecastMid == 0 ~ as.numeric(1),
-    ForecastMid%%9 == 0 ~ as.numeric(ForecastMid/9),
-    ForecastMid%%9 != 0 ~ as.numeric(ForecastMid%/%9 + 1)
+  Forecast <- round(forecast::forecast(dengue.model, xreg = rep(ptval))[["mean"]])
+  Forecast[Forecast<0] <- 0
+  StaffRec <-  dplyr::case_when(
+    Forecast == 0 ~ as.numeric(1),
+    Forecast%%9 == 0 ~ as.numeric(Forecast/9),
+    Forecast%%9 != 0 ~ as.numeric(Forecast%/%9 + 1)
   )
   
-  ForecastHi <- round(forecast::forecast(dengue.model, xreg = rep(ptval))$upper[,2])
-  ForecastHi[ForecastHi<0] <- 0
+  Hi95 <- round(forecast::forecast(dengue.model, xreg = rep(ptval))$upper[,2])
+  Hi95[Hi95<0] <- 0
   StaffHi <-  dplyr::case_when(
-    ForecastHi == 0 ~ as.numeric(1),
-    ForecastHi%%9 == 0 ~ as.numeric(ForecastHi/9),
-    ForecastHi%%9 != 0 ~ as.numeric(ForecastHi%/%9 + 1)
+    Hi95 == 0 ~ as.numeric(1),
+    Hi95%%9 == 0 ~ as.numeric(Hi95/9),
+    Hi95%%9 != 0 ~ as.numeric(Hi95%/%9 + 1)
   )
   
-  print(data.frame(Week,ForecastLo,ForecastMid,ForecastHi,StaffLo,StaffMid,StaffHi))
+  print(data.frame(Week,Lo95,Forecast,Hi95,StaffLo,StaffRec,StaffHi))
 }
 
 
