2cf27f42a6c8ab704e08efbcc87b637e9eb7b248
gwenrino <gwenrino@gmail.com>
2018-05-30 10:51:27 -0700

    Fix broken code in DynReg src files (specify range of actuals to compare to predictions in MAE calculation)

--- dengue/src/Models/4.0.DynamicRegression.R
+++ dengue/src/Models/4.0.DynamicRegression.R
@@ -1,9 +1,15 @@
 ####################################################
 ### Dynamic regression with exogenous regressor  ###
-### 6 week horizon                               ###
-### Mean of future value of xreg variable for fc ###
+####################################################
+### Goal = which variable to use for xreg?       ###
 ####################################################
 
+# For exploration purposes, use 6 week horizon and 
+# mean of future value of xreg variable for forecasting
+
+# Which variable as xreg makes model that best forecasts the last 6 weeks of the series?
+
+# Split train and test sets
 train <- subset(ts.selected, start = 1, end = 930)
 test <- subset(ts.selected, start = 931, end = 936)
 
@@ -14,7 +20,7 @@ v3 <- train[,"reanalysis_tdtr_k"]
 v4 <- train[,"reanalysis_dew_point_temp_k"]
 v5 <- train[,"reanalysis_specific_humidity_g_per_kg"]
 
-# Create xreg objects for forecasts
+# Create xreg objects for forecasts (the mean of the future value of the variable)
 a <- mean(test[,"nonres_guests"])
 b <- mean(test[,"station_max_temp_c"])
 c <- mean(test[,"reanalysis_tdtr_k"])
@@ -40,8 +46,8 @@ ourValue <- "pred"
 
 returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
 
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 9.5
+mean(abs(returnedValues$actuals[437:936] - returnedValues$pred[6,]), na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 11.5
 
 ## MODEL 2: station_max_temp_c as regressor 
 
@@ -62,8 +68,8 @@ ourValue <- "pred"
 
 returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
 
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 8.02
+mean(abs(returnedValues$actuals[437:936] - returnedValues$pred[6,]), na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 3.1
 
 # MODEL 3: reanalysis_tdtr_k as regressor 
 
@@ -84,8 +90,8 @@ ourValue <- "pred"
 
 returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
 
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 8.05
+mean(abs(returnedValues$actuals[437:936] - returnedValues$pred[6,]), na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 3.1
 
 ## MODEL 4: reanalysis_dew_point_temp_k as regressor 
 
@@ -106,8 +112,8 @@ ourValue <- "pred"
 
 returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
 
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 7.98
+mean(abs(returnedValues$actuals[437:936] - returnedValues$pred[6,]), na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 3.0
 
 ## MODEL 5: reanalysis_specific_humidity_g_per_kg as regressor 
 
@@ -128,156 +134,21 @@ ourValue <- "pred"
 
 returnedValues <- ro(x,h=6,origins=500,ourCall,ourValue)
 
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 7.98
+mean(abs(returnedValues$actuals[437:936] - returnedValues$pred[6,]), na.rm = TRUE)
+# MAE of 500 rolling origin cross validation = 3.04
 
-####################################################
-### Dynamic regression with exogenous regressors ###
-### 6 month horizon                              ###
-### Mean of future value of xreg variable for fc ###
-####################################################
-
-train2 <- subset(ts.selected, start = 1, end = 910)
-test2 <- subset(ts.selected, start = 911, end = 936)
-
-# Create xreg objects for models
-v1.1 <- train2[,"nonres_guests"]
-v2.1 <- train2[,"station_max_temp_c"]
-v3.1 <- train2[,"reanalysis_tdtr_k"]
-v4.1 <- train2[,"reanalysis_dew_point_temp_k"]
-v5.1 <- train2[,"reanalysis_specific_humidity_g_per_kg"]
-
-# Create xreg objects for forecasts
-a.1 <- mean(test2[,"nonres_guests"])
-b.1 <- mean(test2[,"station_max_temp_c"])
-c.1 <- mean(test2[,"reanalysis_tdtr_k"])
-d.1 <- mean(test2[,"reanalysis_dew_point_temp_k"])
-e.1 <- mean(test2[,"reanalysis_specific_humidity_g_per_kg"])
-
-## MODEL 1: nonres_guests as regressor
-
-# Fit model, find accuracy of forecast of test set
-train2[,"total_cases"] %>% auto.arima(xreg = v1.1) %>% 
-  forecast(xreg = rep(a.1,26)) %>% 
-  accuracy(test2[,"total_cases"]) # MAE = 12.38
-
-train2[,"total_cases"] %>% auto.arima(xreg = v1.1) %>% summary() # ARIMA(2,0,1)
-
-# Cross validate using ro() function from greybox
-ts.small.1 <- ts.selected[,1:2]
-x <- ts.small.1[,"total_cases"]
-xreg <- ts.small.1[,"nonres_guests"]
-
-ourCall <- "predict(arima(x=data, order=c(2,0,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
-ourValue <- "pred"
-
-returnedValues <- ro(x,h=26,origins=500,ourCall,ourValue)
-
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 21.4
-
-## MODEL 2: station_max_temp_c as regressor
-
-# Fit model, find accuracy of forecast of test set
-train2[,"total_cases"] %>% auto.arima(xreg = v2.1) %>% 
-  forecast(xreg = rep(b.1,26)) %>% 
-  accuracy(test2[,"total_cases"]) # MAE = 34.1
-
-train2[,"total_cases"] %>% auto.arima(xreg = v2.1) %>% summary() # ARIMA(1,1,1)
-
-# Cross validate using ro() function from greybox
-ts.small.2 <- ts.selected[,c(1,3)]
-x <- ts.small.2[,"total_cases"]
-xreg <- ts.small.2[,"station_max_temp_c"]
-
-ourCall <- "predict(arima(x=data, order=c(1,1,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
-ourValue <- "pred"
-
-returnedValues <- ro(x,h=26,origins=500,ourCall,ourValue)
-
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 18.4
-
-# MODEL 3: reanalysis_tdtr_k as regressor
-
-# Fit model, find accuracy of forecast of test set
-train2[,"total_cases"] %>% auto.arima(xreg = v3.1) %>% 
-  forecast(xreg = rep(c.1,26)) %>% 
-  accuracy(test2[,"total_cases"]) # MAE = 35.6
-
-train2[,"total_cases"] %>% auto.arima(xreg = v3.1) %>% summary() # ARIMA(1,1,1)
-
-# Cross validate using ro() function from greybox
-ts.small.3 <- ts.selected[,c(1,4)]
-x <- ts.small.3[,"total_cases"]
-xreg <- ts.small.3[,"reanalysis_tdtr_k"]
-
-ourCall <- "predict(arima(x=data, order=c(1,1,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
-ourValue <- "pred"
-
-returnedValues <- ro(x,h=26,origins=500,ourCall,ourValue)
-
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 18.6
-
-## MODEL 4: reanalysis_dew_point_temp_k as regressor
-
-# Fit model, find accuracy of forecast of test set
-train2[,"total_cases"] %>% auto.arima(xreg = v4.1) %>% 
-  forecast(xreg = rep(d.1,26)) %>% 
-  accuracy(test2[,"total_cases"]) # MAE = 34.2
-
-train2[,"total_cases"] %>% auto.arima(xreg = v4.1) %>% summary() # ARIMA(1,1,1)
-
-# Cross validate using ro() function from greybox
-ts.small.4 <- ts.selected[,c(1,5)]
-x <- ts.small.4[,"total_cases"]
-xreg <- ts.small.4[,"reanalysis_dew_point_temp_k"]
-
-ourCall <- "predict(arima(x=data, order=c(1,1,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
-ourValue <- "pred"
-
-returnedValues <- ro(x,h=26,origins=500,ourCall,ourValue)
-
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 18.0
-
-## MODEL 5: reanalysis_specific_humidity_g_per_kg as regressor
-
-# Fit model, find accuracy of forecast of test set
-train2[,"total_cases"] %>% auto.arima(xreg = v5.1) %>% 
-  forecast(xreg = rep(e.1,26)) %>% 
-  accuracy(test2[,"total_cases"]) # MAE = 34.0
-
-train2[,"total_cases"] %>% auto.arima(xreg = v5.1) %>% summary() # ARIMA(1,1,1)
-
-# Cross validate using ro() function from greybox
-ts.small.5 <- ts.selected[,c(1,6)]
-x <- ts.small.5[,"total_cases"]
-xreg <- ts.small.5[,"reanalysis_specific_humidity_g_per_kg"]
-
-ourCall <- "predict(arima(x=data, order=c(1,1,1), xreg=xreg[counti]), n.ahead=h, newxreg=xreg[counto])"
-ourValue <- "pred"
-
-returnedValues <- ro(x,h=26,origins=500,ourCall,ourValue)
-
-mean(abs(returnedValues$actuals - returnedValues$pred),na.rm = TRUE)
-# MAE of 500 rolling origin cross validation = 18.0
-
-##########################################################################
-### For both 6 week and 26 week horizons, Model 4 seems to be the best ###
-##########################################################################
+#############################################################
+### For both 6 week horizons Model 4 seems to be the best ###
+### ARIMA(1,1,1) with reanalysis_dew_point_temp_k as xreg ###
+#############################################################
 
 model <- auto.arima(ts.selected[,"total_cases"], 
                     xreg = ts.selected[,"reanalysis_dew_point_temp_k"])
 
-###############################################
-### Forecasting reanalysis_dew_point_temp_k ###
-### for use in xreg forecasting             ###
-###############################################
-
-# Model is an ARIMA(1,1,1) with reanalysis_dew_point_temp_k as regressor.
 # In order to forecast total_cases, need forecasts of reanalysis_dew_point_temp_k
+# (In the above exploration, used mean of future values, not forecasts)
+
+# For simplicity, use seasonal naive model to forecast reanalysis_dew_point_temp_k
 
 # Create time series of features for use in model, and train and test sets
 final <- dengue.med[c("total_cases", "reanalysis_dew_point_temp_k")]
@@ -288,22 +159,6 @@ ts.final <- ts(final,
 
 train <- subset(ts.final, start = 1, end = 930)
 test <- subset(ts.final, start = 931, end = 936)
-
-## NAIVE MODEL
-
-# Fit model
-dewpt.mod1 <- naive(ts.final[,"reanalysis_dew_point_temp_k"])
-summary(dewpt.mod1)
-
-train[,"reanalysis_dew_point_temp_k"] %>% naive() %>%
-  forecast(h=6) %>% accuracy(test[,"reanalysis_dew_point_temp_k"])
-# MAE on this test set = 0.64
-
-# Cross validate using tsCV()
-e <- tsCV(ts.final[,"reanalysis_dew_point_temp_k"], forecastfunction = naive, h=6)
-mean(abs(e), na.rm = TRUE) # MAE is 0.81
-
-## SEASONAL NAIVE MODEL
 
 # Fit model
 dewpt.mod2 <- snaive(ts.final[,"reanalysis_dew_point_temp_k"])
@@ -316,6 +171,3 @@ train[,"reanalysis_dew_point_temp_k"] %>% snaive() %>%
 # Cross validate using tsCV()
 e <- tsCV(ts.final[,"reanalysis_dew_point_temp_k"], forecastfunction = snaive, h=6)
 mean(abs(e), na.rm = TRUE) # MAE is 0.78
-
-# Use seasonal naive forecasts of reanalysis_dew_point_temp_k as
-# xreg in forecast of total_cases using ARIMA(1,1,1) model
--- dengue/src/Models/4.1.DynRegValidation.R
+++ dengue/src/Models/4.1.DynRegValidation.R
@@ -1,5 +1,6 @@
 ####################################################
 ### CROSS VALIDATION OF DYNAMIC REGRESSION MODEL ###
+### using snaive forecast of xreg variable       ###   
 ####################################################
 
 # Set up for cross validation using greybox::ro()
@@ -12,26 +13,26 @@ ourValue <- "pred"
 ## 500 rolling origins cross validation with h=1
 
 returnedValues1 <- ro(x,h=1,origins=500,ourCall,ourValue)
-mean(abs(returnedValues1$actuals - returnedValues1$pred),na.rm = TRUE)
+mean(abs(returnedValues1$actuals[437:936] - returnedValues1$pred[1,]), na.rm = TRUE)
 # MAE = 1.1
 
 ## 500 rolling origins cross validation with h=2
 
 returnedValues2 <- ro(x,h=2,origins=500,ourCall,ourValue)
-mean(abs(returnedValues2$actuals - returnedValues2$pred),na.rm = TRUE)
-# MAE = 3.9
+mean(abs(returnedValues2$actuals[437:936] - returnedValues2$pred[2,]), na.rm = TRUE)
+# MAE = 1.9
 
 ## 500 rolling origins cross validation with h=6
 
 returnedValues6 <- ro(x,h=6,origins=500,ourCall,ourValue)
-mean(abs(returnedValues6$actuals - returnedValues6$pred),na.rm = TRUE)
-# MAE = 7.98
+mean(abs(returnedValues6$actuals[437:936] - returnedValues6$pred[6,]),na.rm = TRUE)
+# MAE = 3.0
 
 ## 500 rolling origins cross validation with h=26
 
 returnedValues26 <- ro(x,h=26,origins=500,ourCall,ourValue)
-mean(abs(returnedValues26$actuals - returnedValues26$pred),na.rm = TRUE)
-# MAE = 18.0
+mean(abs(returnedValues26$actuals[437:936] - returnedValues26$pred[26,]),na.rm = TRUE)
+# MAE = 3.35
 
 
 
